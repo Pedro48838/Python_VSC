619b2e035d937c02fa49bb24d6fd358bafc3b69c
Pedro <101953398+Pedro48838@users.noreply.github.com>
2023-08-15 16:40:35 -0300

    Novo commit

--- PYTHON/Exerc_CV/Exerc40__cv.py
+++ PYTHON/Exerc_CV/Exerc40__cv.py
@@ -4,13 +4,39 @@ atingida:
 - Média entre 5.0 e 6.9: RECUPERAÇÃO
 - Média 7.0 ou superior: APROVADO'''
 
+#Primeira maneira de realizar o exercicio
+
+print("||--||" * 12)
+print("Escola de Tangamandapio")
+print("||--||" * 12)
+
+print("Aluno, digite as notas disponiveis.")
+
+n1 = float(input("Primeira nota: "))
+n2 = float(input("Segunda nota: "))
+media = (n1 + n2) / 2
+
+if media > 7.0:
+    print(f"Sua média é de: {media}. Parabens, vc está APROVADO.")
+elif media < 5:
+    print(f"Sua média é de: {media}. Infelizmente você está REPORVADO.")
+elif media >= 5 and media < 7:
+    print(f"Sua média é de: {media}. Você esta de RECUPERAÇÃO.")
 
 
+#Segunda maneira de reaizar o exercicio
 
+nota1 = float(input("Primeira nota: "))
+nota2 = float(input("Segunda nota: "))
+media = (nota1 + nota2) / 2
 
-
-
-
+print(f"Tirando {nota1 :.1f} e {nota2 :.1f}, a média do aluno é {media :.1f}")
+if 7 > media >= 5:
+    print("O aluno está em RECUPERAÇÃO")
+elif media < 5:
+    print("O aluno está REPROVADO")
+elif media >= 7:
+    print("O aluno está APROVADO")
 
 
 
--- PYTHON/Exerc_CV/Exerc41__cv.py
+++ PYTHON/Exerc_CV/Exerc41__cv.py
@@ -5,3 +5,46 @@ de acordo com a idade:
 - Até 19 anos: JÚNIOR
 - Até 25 anos: SÊNIOR
 - Acima de 25 anos: MASTER'''
+
+
+from datetime import date
+
+atual = date.today().year
+nascimento = int(input("Qual o ano de nascimento: "))
+idade = atual - nascimento
+
+print (f"O atleta tem {idade} anos.")
+if idade <= 9:
+    print("Classificação: MIRIM")
+elif idade <= 14:
+    print("Classificação: INFANTIL")
+elif idade <= 19:
+    print("Classificação: JUNIOR")
+elif idade <= 25:
+    print("Classificação: SÊNIOR")
+else:
+    print("Classificação: MASTER")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
